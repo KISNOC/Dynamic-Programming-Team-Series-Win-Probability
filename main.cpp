a3df56feafd7c0720b96b76a179df09d5a71f1fc
Robin <robin.marchand.1@ulaval.ca>
2024-04-07 11:37:47 -0400

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+
+#include "tableau.hpp"
+
+int main() {
+    unsigned int nb_series = 10;
+    float winnning_probability = 0.5;
+    float losing_probability = 1 - winnning_probability;
+
+
+    return 0;
+};
