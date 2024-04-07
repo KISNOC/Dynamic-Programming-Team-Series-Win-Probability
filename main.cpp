553e5b799f757bdc09ca9d8e528cc90447674679
Robin <robin.marchand.1@ulaval.ca>
2024-04-07 11:53:28 -0400

    Table creation

--- main.cpp
+++ main.cpp
@@ -17,6 +17,8 @@ void simulate_series(unsigned int nb_games,
                      float losing_probability,
                      std::vector<unsigned int>& series) {
 
+    unsigned int dimensions[2] = {nb_games + 1, nb_games + 1};
+    Tableau<unsigned int> table(std::vector<unsigned int>(dimensions, dimensions + 2));
 
 
 
