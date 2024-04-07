08678e70eb39b5ca629feea1ff9fe0e9e73755b1
Robin <robin.marchand.1@ulaval.ca>
2024-04-07 11:51:40 -0400

    Rename nb_series to nb_games

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include "tableau.hpp"
 
 int main() {
-    unsigned int nb_series = 10;
+    unsigned int nb_games = 10;
     float winnning_probability = 0.5;
     float losing_probability = 1 - winnning_probability;
 
