2ab29fd693ba2e015b4dbcf2248a1b0e7d53a333
Robin <robin.marchand.1@ulaval.ca>
2024-04-07 12:55:33 -0400

    Modification of array typing

--- main.cpp
+++ main.cpp
@@ -1,24 +1,24 @@
 #include <iostream>
 #include <vector>
+#include <iomanip>
 
 #include "tableau.hpp"
 
 int main() {
-    unsigned int nb_games = 10;
+    unsigned int nb_games = 5;
     float winnning_probability = 0.5;
     float losing_probability = 1 - winnning_probability;
 
 
     return 0;
-};
+}
 
 void simulate_series(unsigned int nb_games,
                      float winnning_probability,
-                     float losing_probability,
-                     std::vector<unsigned int>& series) {
+                     float losing_probability) {
 
     unsigned int dimensions[2] = {nb_games + 1, nb_games + 1};
-    Tableau<unsigned int> table(std::vector<unsigned int>(dimensions, dimensions + 2));
+    Tableau<float> table(std::vector<unsigned int>(dimensions, dimensions + 2));
 
     /* Basic case */
     /* If there are no more games left for team B to win, then team A has loss the series */
